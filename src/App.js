67f5852731f166d1291d8453e10518c07fabc043
Jeevesh0207 <132649861+Jeevesh0207@users.noreply.github.com>
2023-06-22 18:22:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Outlet } from 'react-router-dom'
+import { AuthProvider } from './Components/Auth';
+import Login from './Components/NavComponent/Login';
+import Profile from './Components/NavComponent/Profile';
+import Home from './Components/Home';
+import {ProfileReqAuth} from './Components/NavComponent/ProfileReqAuth'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path='profile' element=
+            {
+              <ProfileReqAuth>
+                <Profile />
+              </ProfileReqAuth>
+            }
+          />
+        </Routes>
+      
+        </div>
+    </AuthProvider>
+
   );
 }
 
